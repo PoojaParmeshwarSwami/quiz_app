a0af674ffd49910046a13e7fe466d79ac147b6e9
PoojaParmeshwarSwami <poojaswami8316@gmail.com>
2024-02-12 15:42:08 +0530

    option onclick color change

--- js/utilities.js
+++ js/utilities.js
@@ -5,9 +5,9 @@ const activebtn = document.querySelector("#activebtn");
 const buttons_container = document.querySelector(".buttons_container");
 const qNo = document.querySelector("#qNo");
 const qtn = document.querySelector("#qtn");
-
+const optionContainer =document.querySelector(".optionContainer")
+const option = document.querySelector(".options")
 const options = document.querySelectorAll(".options")
-
 
 export function createbtn(val) {
     const btn = document.createElement("button");
@@ -20,11 +20,11 @@ export function createbtn(val) {
     });
     buttons_container.appendChild(btn);
   }
-
+ 
   
   export function display(disp) {
     qtn.innerHTML = questionArray[disp].question;
-    console.log(options)
+    // console.log(options)
     options.forEach((element,ind) => {
       element.innerHTML = questionArray[disp].option[ind]
     });
@@ -41,4 +41,28 @@ export function change_btn_color(arg) {
       }
     }
   }
+
+  optionContainer.addEventListener("click",function(e){
+    change_opt_color(e.target)
+    });
+
+  
+ export function change_opt_color(opt){
+
+  let optionList = optionContainer.children;
+  
+  for(let i=0;i< optionList.length;i++){
+    
+    if(optionList[i]===opt){
+      optionList[i].style.backgroundColor = "green";
+    }
+    else {
+      optionList[i].style.backgroundColor = "white";
+    }
+  }
+
+  }
+
+
+  
   
